5a4ca76c11db56c80d27a03bf85c8dcd7cf4a658
Cristina-F-R <cristina.faixedas@gmail.com>
2023-02-23 19:46:35 +0100

    add if noImg

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,31 @@
 'use strict';
 
 const serverUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+const noImgUrl ='https://via.placeholder.com/200x200/3d1b5b/f3f3f3/?text=cocktail';
 let cocktailData = [];
 const listSearch = document.querySelector('.js-listElement');
 const inputSearch = document.querySelector('.js-inputSearch');
 const btnSearch = document.querySelector('.js-btnSearch');
 
+//CARGA LISTADO DE MARGARITAS POR DEFECTO
 fetch(serverUrl)
     .then((response) => response.json())
     .then((data) =>{
-        // console.log(data.drinks);
-        cocktailData = data.drinks.map((drink) => ({
+        if(data.drinks.strDrinkThumb === ''){
+            cocktailData = data.drinks.map((drink) => ({
+            name: drink.strDrink,
+            img: noImgUrl,
+        }));
+        render(cocktailData);
+        }else{cocktailData = data.drinks.map((drink) => ({
             name: drink.strDrink,
             img: drink.strDrinkThumb,
         }));
-        // console.log(cocktailData);
-        render(cocktailData);
+        render(cocktailData);}
+        
 });
 
+//RENDERIZADO DEL LISTADO PRINCIPAL DE BEBIDAS
 function render(){
     listSearch.innerHTML = '';
     for (const cocktail of cocktailData ){
@@ -37,18 +45,23 @@ function render(){
     }
 };
 
+//FUNCIÓN DE BÚSQUEDA
 function handleclick () {
     let inputValue = inputSearch.value;
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputValue}`)
         .then((response) => response.json())
         .then((data) =>{
-            console.log(data);
-            cocktailData = data.drinks.map((cocktail) => ({
-                name: cocktail.strDrink,
-                img: cocktail.strDrinkThumb,
-            }));
-            // console.log(cocktailData);
-            render(cocktailData);
+         if(data.drinks.strDrinkThumb === ''){
+            cocktailData = data.drinks.map((drink) => ({
+            name: drink.strDrink,
+            img: noImgUrl,
+        }));
+        render(cocktailData);
+        }else{cocktailData = data.drinks.map((drink) => ({
+            name: drink.strDrink,
+            img: drink.strDrinkThumb,
+        }));
+        render(cocktailData);}
     });
 }
 
